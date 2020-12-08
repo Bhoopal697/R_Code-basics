433def966dbf2399e6498c7200b919fd626382de
Bhoopal Reddy R V <72329276+Bhoopal697@users.noreply.github.com>
2020-12-08 22:13:05 +0530

    Add files via upload

---
+++ Worldphones.R
@@ -0,0 +1,16 @@
+harshi <- datasets::WorldPhones
+# to find first 10 rows
+head(WorldPhones)
+# to find last 7 rows
+tail(WorldPhones)
+# to find summary of the worldPhones
+summary(WorldPhones)
+# plotting
+plot(WorldPhones,col='blue')
+# bar plot
+barplot(WorldPhones,col = 'orange')
+# histogram
+hist(WorldPhones)
+hist(WorldPhones,col = 'red')
+# box plot
+boxplot(WorldPhones,col='yellow')
